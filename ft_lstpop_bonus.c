ca7882222701fe116faf358797461e576dfa76e9
imongeon/Ian Antony Mongeon Charest <ian.mongeon42@gmail.com>
2022-03-27 23:52:47 -0400

    libft - ft_lstpop done, yet untested - attempt0

--- ft_lstpop_bonus.c
+++ ft_lstpop_bonus.c
@@ -3,6 +3,7 @@
 void	*ft_lstpop(t_list **lst, unsigned int index)
 {
 	t_list	*elem;
+	t_list	*elem_swap;
 	void	*content;
 
 	if (!(*lst))
@@ -10,7 +11,7 @@ void	*ft_lstpop(t_list **lst, unsigned int index)
 	elem = *lst;
 	if (index == 0)
 	{
-		*lst = (*lst)->next;
+		*lst = elem->next;
 		content = elem->content;
 		free(elem);
 		return (content);
@@ -19,13 +20,9 @@ void	*ft_lstpop(t_list **lst, unsigned int index)
 		elem = elem->next;
 	if (index)
 		return (NULL);
-	else
-	{
-		content = elem->next->content;
-		if (elem->next->next)
-			elem->next = elem->next->next;
-		else
-		free(elem->next);
-	}
+	elem_swap = elem->next;
+	content = elem_swap->content;
+	elem->next = elem->next->next;
+	free(elem_swap);
 	return (content);
 }
